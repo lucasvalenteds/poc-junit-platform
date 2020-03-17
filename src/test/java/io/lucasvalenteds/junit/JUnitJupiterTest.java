c0bb2afb513f692bbadff798028ea18e73a9ac60
Lucas <lucasvalenteds@gmail.com>
2020-03-16 22:14:26 -0300

    Add source code

---
+++ src/test/java/io/lucasvalenteds/junit/JUnitJupiterTest.java
@@ -0,0 +1,12 @@
+package io.lucasvalenteds.junit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JUnitJupiterTest {
+
+    @Test
+    public void testRunningWithJunitJupiter() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/io/lucasvalenteds/junit/JUnitVintageTest.java
@@ -0,0 +1,12 @@
+package io.lucasvalenteds.junit;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class JUnitVintageTest {
+
+    @Test
+    public void testRunningWithJunitVintage() {
+        assertTrue(true);
+    }
+}
