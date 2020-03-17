3e4fa33f5c9e88566b1b5929af6ee3824c573fe7
Lucas <lucasvalenteds@gmail.com>
2020-03-16 22:27:23 -0300

    Bump JDK version from 1.8 to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks.withType<Test> {
